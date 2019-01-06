35e53b29b30b7f17cd3fab5b1965327d08762f1a
sarken <sarken@gmail.com>
2019-01-06 04:58:07 -0500

    AO3-4949 One more stab at seeing if the item is approved by the collection

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -298,7 +298,7 @@ class CollectionItem < ApplicationRecord
   def notify_of_reveal
     # Don't include the collection name in the email if the collection has not
     # approved the item.
-    collection_id = if self.approved_by_collection?
+    collection_id = if approved_by_collection?
                       self.collection.id
                     else
                       nil
