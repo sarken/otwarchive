42204abdb3c0b26eec3d0342c44283d4e588056f
sarken <sarken@gmail.com>
2020-02-26 20:17:39 -0500

    Create a new InboxComment for each subscriber a SubscriptionAlert applies to

---
+++ app/models/subscription_alert.rb
@@ -0,0 +1,21 @@
+class SubscriptionAlert < ApplicationRecord
+  validates_presence_of :creation_id
+  validates_presence_of :creation_type
+
+  belongs_to :creation, polymorphic: true
+
+  # Each user with a relevant subscription gets a new inbox comment. This way,
+  # users can delete them or mark them as read on an individual basis.
+  after_create :create_inbox_comments
+  def create_inbox_comments
+    subscriber_ids.each do |subscriber_id|
+      InboxComment.create(item_type: self.class, feedback_comment_id: id,
+        user_id: subscriber_id)
+    end
+  end
+
+  def subscriber_ids
+    work = creation.respond_to?(:work) ? creation.work : creation
+    Subscription.for_work(work).pluck(:user_id)
+  end
+end
