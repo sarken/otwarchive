0ef5c457656949fd742361fb5cbb32ec124fe399
sarken <sarken@gmail.com>
2021-09-01 02:55:44 -0400

    Fix subject

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -713,7 +713,7 @@ describe UserMailer do
       it_behaves_like "an email with a valid sender"
 
       it "has the correct subject line" do
-        subject = "[#{ArchiveConfig.APP_SHORT_NAME}] A gift work for you "
+        subject = "[#{ArchiveConfig.APP_SHORT_NAME}] A gift work for you"
         expect(email).to have_subject(subject)
       end
 
